fa0e60ec980b017f09763e70bd612c8a86fd4d45
unknown <michaeljunior794@gmail.com>
2023-12-26 16:33:49 +0000

    filter

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -8,25 +8,23 @@ import { useDispatch } from "react-redux";
 import { cartActions } from '../redux/cartRedux';
 
 const ProductList = () => {
-    const [loading, setLoading] = useState(true);
-    const [products, setProducts] = useState([]);
-    
-    const [editingProduct, setEditingProduct] = useState(null);
-    const [editProductModal, setEditProductModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [editingProduct, setEditingProduct] = useState(null);
+  const [editProductModal, setEditProductModal] = useState(false);
+  const [productId, setProductId] = useState(null);
+  const [deleteProductModal, setDeleteProductModal] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(""); // New state variable for selected category
+  const dispatch = useDispatch();
 
-    const [productId, setProductId] = useState(null);
-    const [deleteProductModal, setDeleteProductModal] = useState(false);
+  const handleAddToCart = (product) => {
+    dispatch(cartActions.addToCart({ ...product, quantity: 1 }));
+  }
 
-    const [refresh, setRefresh] = useState(false);
-    const dispatch = useDispatch();
-
-    const handleAddToCart = (product) => {
-        dispatch(cartActions.addToCart({ ...product, quantity: 1 }));
-    }
-
-    const refreshList = () => {
-        setRefresh(prev => !prev);
-    }
+  const refreshList = () => {
+    setRefresh(prev => !prev);
+  }
 
   const handleEdit = (product) => {
     setEditingProduct(product)
@@ -38,55 +36,67 @@ const ProductList = () => {
     setDeleteProductModal(true);
   };
 
-  useEffect(()=>{
-    fetch(`${import.meta.env.VITE_API_URL}/product/get/all`).then(response=>response.json()).then((result)=>{
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/product/get/all`).then(response => response.json()).then((result) => {
       setLoading(false);
       setProducts(result);
-    })     
-    },[refresh]);
+    })
+  }, [refresh]);
 
-    return (
-        <div >
-         <Link to={"/product/create"}><h4>Add New</h4></Link>
-         {loading && <h3>LOADING!!</h3>}
-         {!loading &&  <table className='product-table'>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>SKU</th>
-                <th>Description</th>
-                <th>Category</th>
-                <th>Price</th>
-                <th></th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-      <tbody>
-        {products.map(product => (
-          <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.sku}</td>
-            <td>{product.description}</td>
-            <td>{product.category}</td>
-            <td>{product.price}</td>
-            <td><button onClick={()=>{handleEdit(product)}}>Edit</button></td>
-            <td><button onClick={() => handleDelete(product.id)}>Delete</button></td>
-            <td><button  onClick={() => handleAddToCart(product)} >Add to Cart</button></td>
-            </tr>
+  // Filter products based on selected category
+  const filteredProducts = selectedCategory ? products.filter(product => product.category === selectedCategory) : products;
+
+  return (
+    <div>
+      <Link to={"/product/create"}><h4>Add New Product🤺</h4></Link>
+
+      <select className="product-select" name="category" id="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+        <option value="">All</option>
+        {[...new Set(products.map((product) => product.category))].map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
         ))}
-      </tbody>
-    </table>}
+      </select>
 
-    {editProductModal && <EditProductModal 
-    product={editingProduct} 
-    onClose={ ()=>{setEditProductModal(false); refreshList(); } }/>}
-    {deleteProductModal && <DeleteModal
-     productId={productId} 
-     onClose={()=>{setDeleteProductModal(false); refreshList(); } }/>}
-     </div>
+      {loading && <h3>LOADING!!</h3>}
+      {!loading && <table className='product-table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>SKU</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredProducts.map(product => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.sku}</td>
+              <td>{product.description}</td>
+              <td>{product.category}</td>
+              <td>{product.price}</td>
+              <td><button onClick={() => { handleEdit(product) }}>Edit</button></td>
+              <td><button onClick={() => handleDelete(product.id)}>Delete</button></td>
+              <td><button onClick={() => handleAddToCart(product)}>Add to Cart</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>}
 
+      {editProductModal && <EditProductModal
+        product={editingProduct}
+        onClose={() => { setEditProductModal(false); refreshList(); }} />}
+      {deleteProductModal && <DeleteModal
+        productId={productId}
+        onClose={() => { setDeleteProductModal(false); refreshList(); }} />}
+    </div>
   );
 }
- 
+
 export default ProductList;
